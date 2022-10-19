b2167c0de1e5100a6c0ec03b7180ef4d2f5148af
Reyudas <tintinvougeot@gmail.com>
2022-10-19 18:53:50 +0200

    added vinyl carrousel

---
+++ platinum-front-end/src/components/vinylCard.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+interface vinylCardProps {
+    imgPath: string,
+    title: string,
+    text: string,
+    price: string,
+    href: string
+}
+
+const VinylCard = (props: vinylCardProps) => {
+    return (
+        <Card sx={{ maxWidth: 1080, margin: 2 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={props.imgPath}
+                    alt="album cover"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.text}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+
+export default VinylCard;
--- platinum-front-end/src/pages/homePage.tsx
+++ platinum-front-end/src/pages/homePage.tsx
@@ -1,26 +1,64 @@
-import React, { Component } from "react";
+import React, { Component, useState, useRef } from "react";
 
-import logo from '../logo.svg';
-import '../App.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import VinylCard from "../components/vinylCard";
+import { IconButton } from '@mui/material';
+import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
-function HomePage() {
+
+const handleDragStart = (e: { preventDefault: () => any; }) => e.preventDefault();
+
+const items = [
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+    <VinylCard imgPath={"https://img.freepik.com/free-psd/vinyl-record-cover-with-orange-background-mockup_23-2149379034.jpg?w=2000"} title={"album 1"} text={"le dernier album de artiste 1"} price={"55$"} href={"/album"} />,
+];
+
+const responsive = {
+    0: { items: 1 },
+    1024: { items: 4 },
+}
+
+const HomePage = () => {
+
+    const RenderNextButton = () => {
+        return (
+            <IconButton style={{ position: "absolute", right: 0, top: "100px" }}>
+                <ArrowForwardIosRoundedIcon />
+            </IconButton>
+        )
+    };
+
+    const RenderPrevButton = () => {
+        return (
+            <IconButton style={{ position: "absolute", left: 0, top: "100px" }}>
+                <ArrowBackIosRoundedIcon />
+            </IconButton>
+        )
+    };
+
     return (
-        <div className="App" >
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div >
+        <AliceCarousel
+            responsive={responsive}
+            mouseTracking
+            disableDotsControls
+            items={items}
+            paddingLeft={20}
+            paddingRight={20}
+            keyboardNavigation={true}
+            renderPrevButton={() => {
+                return <RenderPrevButton />
+            }}
+            renderNextButton={() => {
+                return <RenderNextButton />
+            }}
+        />
     )
 
 }
