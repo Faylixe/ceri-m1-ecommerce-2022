c235c5aeed73511c5bc5a813dae162bfe7e9cd42
Reyudas <tintinvougeot@gmail.com>
2022-10-19 17:05:26 +0200

    added appbar

--- platinum-front-end/src/App.tsx
+++ platinum-front-end/src/App.tsx
@@ -5,12 +5,13 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import SearchAppBar from './components/searchAppBar';
 import HomePage from './pages/homePage';
 
 function App() {
   return (
     <>
-      <p>salut</p>
+      <SearchAppBar />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
