64f6069031453ef0a063de526d0c717aa662332e
lydia soltani <lydia.soltani@alumni.univ-avignon.fr>
2022-10-31 17:45:09 +0100

    Lien Front et Back début

--- appli_ecommerce/src/app/app-routing.module.ts
+++ appli_ecommerce/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/header/header.component';
-
+import { BodyComponent } from 'src/app/body/body.component';
 
 
 const routes: Routes = [
   {path: '' , component : AppComponent},
   {path: 'header' , component : HeaderComponent},
-
+  {path: 'body' , component : BodyComponent},
 
 
 ];
