7bb6b3c011103576bbceda6afd0c2da8c127b89f
Reyudas <tintinvougeot@gmail.com>
2022-12-12 10:37:12 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,11 +26,13 @@ resource "google_cloud_run_service" "backend" {
     spec {
       service_account_name = "terraform-orangedog@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/orangedog/back-end"
         env {
           name = "DATABASE_ADDRESS"
           value_from {
             secret_key_ref {
               name = "data_google_secret_manager_secret address secret_id"
+              key = "latest"
             }
           }
         }
