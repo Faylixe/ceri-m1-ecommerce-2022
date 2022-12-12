d81be2c9700130d41e799e72b10ed6e0fe2b9a6c
Reyudas <tintinvougeot@gmail.com>
2022-12-12 10:29:35 +0100

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,29 @@ provider "google" {
     project = "ceri-m1-ecommerce-2022"
     region = "europe-west1"
 }
+
+data "google_secret_manager_secret" "address" {
+  secret_id = "mysql.address"
+}
+
+resource "google_cloud_run_service" "backend" {
+
+  name     = "backend"
+  location = "europe-west1"
+
+  template {
+    spec {
+      service_account_name = "terraform-orangedog@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
+      container {
+        env {
+          name = "DATABASE_ADDRESS"
+          value_from {
+            secret_key_ref {
+              name = "data_google_secret_manager_secret address secret_id"
+            }
+          }
+        }
+      }
+    }
+  }
+}
