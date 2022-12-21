91a4360bbd63056f80678606bf30f4fc1681ecde
ElodieGiry <71441548+ElodieGiry@users.noreply.github.com>
2022-12-21 13:53:39 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "google_cloud_run_service" "backend" {
                     name="USER"
                     value_from{
                         secret_key_ref{
-                            name=data_google_secret_manager_secret.user.secret_id
+                            name=data.google_secret_manager_secret.user.secret_id
                             key="latest"
                         }
                     }
@@ -63,7 +63,7 @@ resource "google_cloud_run_service" "backend" {
                     name="HOST"
                     value_from{
                         secret_key_ref{
-                            name=data_google.secret_manager_secret.host.secret_id
+                            name=data.google.secret_manager_secret.host.secret_id
                             key="latest"
                         }
                     }
@@ -76,7 +76,7 @@ resource "google_cloud_run_service" "backend" {
                     name="DBNAME"
                     value_from{
                         secret_key_ref{
-                            name=data_google.secret_manager_secret.dbname.secret_id
+                            name=data.google.secret_manager_secret.dbname.secret_id
                             key="latest"
                         }
                     }
