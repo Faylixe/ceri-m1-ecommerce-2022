f21023b2ee9d601aedf9e21aa48643e364a75718
Reyudas <tintinvougeot@gmail.com>
2023-01-03 15:41:24 +0100

    ajout de la conf front-end

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,28 @@ resource "google_cloud_run_service" "backend" {
             }
           }
         }
+        env {
+          name = "DATABASE_NAME"
+          value_from {
+            secret_key_ref {
+              name = data.google_secret_manager_secret.database.secret_id
+              key = "latest"
+            }
+          }
+        }
+        env {
+          name = "PASSWORD"
+          value_from {
+            secret_key_ref {
+              name = data.google_secret_manager_secret.password.secret_id
+              key = "latest"
+            }
+          }
+        }
+        env {
+          name = "DATABASE_USER"
+          value = "orangedog"
+        }
         ports {
           container_port = 5000
         }
@@ -77,6 +99,39 @@ output "url" {
   sensitive   = false
   description = "description"
   depends_on  = []
+}
+
+resource "google_cloud_run_service" "frontend" {
+
+  name     = "orangedog-frontend"
+  location = "europe-west1"
+
+  template {
+    spec {
+      service_account_name = "terraform-orangedog@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/orangedog/front-end"
+        env {
+          name = "BACKEND_URL"
+          value = google_cloud_run_service.backend.status[0].url
+        }
+        ports {
+          container_port = 80
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "1"
+      }
+    }
+  }
+
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
 }
 
 resource "google_cloud_run_service_iam_member" "invokers" {
