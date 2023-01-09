2390a72f9983afac00dd8eb4546cff83c18b4598
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2023-01-09 16:15:45 +0100

    Update connexion.py

--- back/connexion.py
+++ back/connexion.py
@@ -2,20 +2,9 @@ import mariadb
 import hashlib
 import identifiantsbdd
 
-USER=environ.get("USER")
-PASSWORD=environ.get("PASSWORD")
-DBNAME=environ.get("DBNAME")
-HOST=environ.get("HOST")
-MYSQL_PORT=3306
 
-connection = mariadb.connect(user=USER, password=PASSWORD, database=DBNAME, host=HOST, port=MYSQL_PORT)
-cursorDatabase = connection.cursor()
-
-# p=os.system('start cloud-sql-proxy.exe --credentials-file "Cle GCP.json" ceri-m1-ecommerce-2022:europe-west1:mysql-primary')
-# print(p)
-
-# connection = mariadb.connect(user=identifiantsbdd.username, password=identifiantsbdd.password, database=identifiantsbdd.database, host=identifiantsbdd.host, port=identifiantsbdd.port)
-cursorDatabase = connection.cursor()
+import database
+cursorDatabase = database.connection.cursor()
 
 
 def inscription(nom, prenom, email, password, telephone, adresse, codePostal, ville, pays):
@@ -24,7 +13,7 @@ def inscription(nom, prenom, email, password, telephone, adresse, codePostal, vi
     password=result.hexdigest()
     query=f'INSERT INTO `client`(`nomClient`, `prenomClient`, `emailClient`, `mdpClient`, `telephoneClient`, `adresseClient`, `cpClient`, `villeClient`, `paysClient`) VALUES (\'{nom}\',\'{prenom}\',\'{email}\',\'{password}\', \'{telephone}\',\'{adresse}\',\'{codePostal}\',\'{ville}\',\'{pays}\')'
     cursorDatabase.execute(query)
-    connection.commit()
+    database.connection.commit()
 
 def connexion(email, password):
     encoded=password.encode()
@@ -34,7 +23,7 @@ def connexion(email, password):
     cursorDatabase.execute(query)
     result=cursorDatabase.fetchall()
     if len(result) == 1:
-        query=f'SELECT prenomClient, nomClient FROM `client` WHERE emailClient = \'{email}\' AND mdpClient = \'{password}\''
+        query=f"SELECT prenomClient, nomClient FROM `client` WHERE emailClient = '{email}' AND mdpClient = '{password}'"
         cursorDatabase.execute(query)
         result=cursorDatabase.fetchall()
         return('Bonjour ' + result[0][0]+ ' ' + result[0][1])
