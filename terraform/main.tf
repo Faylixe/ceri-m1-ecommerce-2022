5386776ae0fb1e30abdc01300bcb4a70270fd5ac
ElodieGiry <71441548+ElodieGiry@users.noreply.github.com>
2023-01-09 15:17:50 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "google_cloud_run_service" "backend" {
         spec{
             service_account_name = "terraform-whitehorse@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
             containers{
-                image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/whitehorse/backend:0.0.3"
+                image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/whitehorse/backend:0.0.4"
                 env{
                     name="USER"
                     value_from{
@@ -110,7 +110,7 @@ resource "google_cloud_run_service" "frontend" {
     spec {
       service_account_name = "terraform-whitehorse@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/whitehorse/frontend:0.0.3"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/whitehorse/frontend:0.0.4"
         env {
           name = "BACKEND_URL"
           value = google_cloud_run_service.backend.status.0.url
