1487f190bfe41527534e1322024c66cc91a205c2
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2023-01-11 14:00:55 +0100

    Update database.py

--- back/database.py
+++ back/database.py
@@ -12,7 +12,7 @@ MYSQL_PORT=3306
 
 try:
    connection = mariadb.connect(user=USER, password=PASSWORD, database=DBNAME, unix_socket= "/cloudsql/ceri-m1-ecommerce-2022:europe-west1:mysql-primary", port=int(MYSQL_PORT))
-    print("Connexion à la bdd réussie")
+   print("Connexion à la bdd réussie")
 except mariadb.Error as e:
     print(f"Impossible de se connecter à la bdd : {e}")
     sys.exit(1)
